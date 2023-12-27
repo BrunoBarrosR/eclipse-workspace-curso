b85e27d9de2fbb4d5948385d0169a40a8a7a93c3
BrunoBarrosR <bruno_barros@hotmail.com.br>
2023-12-27 09:41:04 -0300

    Atualização

--- exercicios01/src/exercicios01_05/Main.java
+++ exercicios01/src/exercicios01_05/Main.java
@@ -9,10 +9,10 @@ public class Main {
 		Locale.setDefault(Locale.US); 
 		Scanner sc = new Scanner(System.in);
 		
-		int codpec1 = sc.nextInt();
+//		int codpec1 = sc.nextInt();
 		int numpec1 = sc.nextInt();
 		double valor1 = sc.nextDouble();
-		int codpec2 = sc.nextInt();
+//		int codpec2 = sc.nextInt();
 		int numpec2 = sc.nextInt();
 		double valor2 = sc.nextDouble();
 		double valorTotal = (numpec1 * valor1 + numpec2 * valor2);
